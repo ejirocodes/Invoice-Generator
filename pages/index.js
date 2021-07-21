c9594f492c8d1adc05cca2354d3f97a79c0488a2
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-07-21 17:28:39 +0100

    hook invoice form

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useReducer, useRef, useState } from 'react';
 
 export default function Home() {
   const initialState = {
@@ -19,6 +19,13 @@ export default function Home() {
 
   const [formFields, dispatch] = useReducer(reducer, initialState);
   const [total, setTotal] = useState(0);
+  const [invoiceFields, setInvoiceFields] = useState([
+    {
+      itemDescription: '',
+      qty: '',
+      price: '',
+    },
+  ]);
 
   const handleInputChange = (e) => {
     dispatch({ field: e.target.name, value: e.target.value });
@@ -28,10 +35,38 @@ export default function Home() {
     return setTotal(formFields.quantity * formFields.unitPrice);
   };
 
+  const addInvoiceItem = () => {
+    setInvoiceItems([
+      ...invoiceItems,
+      {
+        itemDescription: '',
+        rate: '',
+        price: '',
+      },
+    ]);
+  };
+
+  const handleChange = (index, event) => {
+    const values = [...invoiceFields];
+    console.log(event.target.name);
+    if (event.target.name === 'itemDescription') {
+      values[index].itemDescription = event.target.value;
+    } else if (event.target.name === 'qty') {
+      values[index].qty = event.target.value;
+    } else if (event.target.name === 'price') {
+      values[index].price = event.target.value;
+    }
+    setInvoiceFields(values);
+  };
+
   useEffect(() => {
     getTotal();
-    console.log(total);
-  }, [total, formFields.quantity, formFields.unitPrice]);
+  }, [
+    total,
+    formFields.quantity,
+    formFields.unitPrice,
+    formFields.invoiceItem,
+  ]);
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -41,7 +76,7 @@ export default function Home() {
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 my-12">
-        <div className="w-full max-w-6xl">
+        <div className="w-full max-w-xl">
           <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div className="mb-4">
               <label
@@ -104,53 +139,59 @@ export default function Home() {
                 onChange={handleInputChange}
               />
             </div>
-            <div className="flex">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2 w-full mr-8"
-                htmlFor="invoiceItem"
-              >
-                Invoice Item
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="invoiceItem"
-                  name="invoiceItem"
-                  type="text"
-                  spellCheck="false"
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2 w-full mr-8"
-                htmlFor="quantity"
-              >
-                Quantity
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="quantity"
-                  name="quantity"
-                  type="number"
-                  spellCheck="false"
-                  onChange={handleInputChange}
-                />
-              </label>
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2 w-full"
-                htmlFor="unitPrice"
-              >
-                Unit Price
-                <input
-                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                  id="unitPrice"
-                  name="unitPrice"
-                  type="tel"
-                  spellCheck="false"
-                  onChange={handleInputChange}
-                />
-              </label>
-            </div>
+            {invoiceFields.map((invoiceField, i) => (
+              <div className="flex" key={`${invoiceField}~${i}`}>
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2 w-full mr-8"
+                  htmlFor="itemDescription"
+                >
+                  Invoice Item
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="itemDescription"
+                    name="itemDescription"
+                    type="text"
+                    spellCheck="false"
+                    value={invoiceField.itemDescription}
+                    onChange={(event) => handleChange(i, event)}
+                  />
+                </label>
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2 w-full mr-8"
+                  htmlFor="quantity"
+                >
+                  Quantity
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="quantity"
+                    name="qty"
+                    type="number"
+                    spellCheck="false"
+                    value={invoiceField.qty}
+                    onChange={(event) => handleChange(i, event)}
+                  />
+                </label>
+                <label
+                  className="block text-gray-700 text-sm font-bold mb-2 w-full"
+                  htmlFor="unitPrice"
+                >
+                  Unit Price
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                    id="unitPrice"
+                    name="price"
+                    type="tel"
+                    spellCheck="false"
+                    value={invoiceField.price}
+                    onChange={(event) => handleChange(i, event)}
+                  />
+                </label>
+              </div>
+            ))}
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
               type="button"
+              // onClick={addInvoiceItem}
             >
               Add Item
             </button>
