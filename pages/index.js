9e7220e8d9d55bb692796c5732e12d420050f4c0
Ejiro Asiuwhu <ejiroasiuwhu10@gmail.com>
2021-07-21 10:38:50 +0100

    finish invoice form

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,24 @@
 import Head from 'next/head';
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 
 export default function Home() {
   const initialState = {
     billTo: '',
     shipTo: '',
+    dueDate: '',
+    note: '',
   };
 
-  function reducer(state, { field, value }) {
+  function reducer(state = initialState, { field, value }) {
     return { ...state, [field]: value };
   }
+
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [total, setTotal] = useState(0);
 
   const handleInputChange = (e) => {
     dispatch({ field: e.target.name, value: e.target.value });
+
     console.log(state);
   };
 
@@ -24,60 +29,151 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <div className="w-full max-w-xs">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20">
+        <div className="w-full max-w-6xl">
           <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
             <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="sender"
+              >
+                Your email address
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="sender"
+                name="sender"
+                type="email"
+                required
+                placeholder="Who is this invoice from? (required)"
+                onChange={handleInputChange}
+              />
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 htmlFor="billTo"
               >
                 Bill To
               </label>
-              <input
+              <textarea
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="billTo"
                 name="billTo"
-                type="text"
+                type="email"
+                required
                 placeholder="Who is this invoice to? (required)"
                 onChange={handleInputChange}
+              />
+              {state.billTo}
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="shipTo"
+              >
+                Ship To
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="shipTo"
+                type="text"
+                required
+                placeholder="******************"
               />
             </div>
             <div className="mb-6">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
+                htmlFor="dueDate"
               >
-                Password
+                Due Date
               </label>
               <input
-                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="******************"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="dueDate"
+                type="date"
+                onChange={handleInputChange}
               />
-              <p className="text-red-500 text-xs italic">
-                Please choose a password.
-              </p>
+            </div>
+            <div className="flex">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 w-full mr-8"
+                htmlFor="invoiceItem"
+              >
+                Invoice Item
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="invoiceItem"
+                  type="text"
+                  spellCheck="false"
+                  onChange={handleInputChange}
+                />
+              </label>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 w-full mr-8"
+                htmlFor="quantity"
+              >
+                Quantity
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="quantity"
+                  type="number"
+                  spellCheck="false"
+                  onChange={handleInputChange}
+                />
+              </label>
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2 w-full"
+                htmlFor="unitPrice"
+              >
+                Unit Price
+                <input
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                  id="unitPrice"
+                  type="tel"
+                  spellCheck="false"
+                  onChange={handleInputChange}
+                />
+              </label>
+            </div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Add Item
+            </button>
+            <div className="my-6 flex flex-col">
+              <label
+                htmlFor="note"
+                className="block text-gray-700 text-sm font-bold mb-2 w-full"
+              >
+                Invoice Notes
+              </label>
+              <textarea
+                id="note"
+                name="note"
+                onChange={handleInputChange}
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              />
+            </div>
+            <div className="mb-6 flex justify-between font-bold text-xl">
+              <p>Total:</p>
+              <p>{total}</p>
             </div>
             <div className="flex items-center justify-between">
               <button
                 className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="button"
               >
-                Sign In
+                Send Invoice
               </button>
-              <a
-                className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
-                href="#"
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
               >
-                Forgot Password?
-              </a>
+                Download Invoice
+              </button>
             </div>
           </form>
-          <p className="text-center text-gray-500 text-xs">
-            &copy;2020 Acme Corp. All rights reserved.
-          </p>
         </div>
       </main>
 
